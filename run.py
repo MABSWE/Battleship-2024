9fdd79864b39c560126c153fcefcdd033e802ca1
MABSWE <>
2024-01-22 19:48:18 +0000

    Make structure of the workplace by adding functions as a guideline

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+Import random
+
+# Function to create the game board
+
+# Function for players turn
+
+# Function for guess for the row and col
+
+# Function to see if the guess contains a ship
+
+# Function for computers turn
+
+# Function to display the rules of the game
+
+# Main logic
